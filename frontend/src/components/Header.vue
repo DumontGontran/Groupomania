6312741718c7d2a42831aee988b353c40a6e6e70
DumontGontran <dumont.gontran@orange.fr>
2022-05-13 05:06:33 +0200

    mise en place du header pour les composants de fil d'actualité

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,13 +1,18 @@
 <template>
-  <header>
+  <header class="flex flex-row flex-between">
     <img class="logo" src="../assets/logo/icon-left-font.svg" alt="Logo Groupomania"/>
+    <ul class="flex flex-row">
+      <li><router-link class="navLink" to="/public_feed">Fil d'actualité</router-link></li>
+      <li><router-link class="navLink" to="/personal_feed">Fil personnel</router-link></li>
+      <li><router-link class="navLink" to="/profil">Profil</router-link></li>
+      <li><router-link class="navLink" to="/login">Déconnexion</router-link></li>
+    </ul>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header-component",
-  props: {},
+  name: "Header",
 };
 </script>
 
@@ -17,7 +22,21 @@ border-bottom: 1px solid black;
 }
 
 .logo{
-    width: 200px;
-    height: 200px;
+    width: 150px;
+    height: 150px;
+}
+
+.navLink{
+  margin: 0;
+  padding:0;
+  margin-right: 20px;
+  margin-left: 50px;
+  text-decoration: none;
+  color: black;
+  &:hover{
+    text-decoration: underline;
+    text-decoration-color: red;
+    text-decoration-thickness: 50%;
+  }
 }
 </style>
